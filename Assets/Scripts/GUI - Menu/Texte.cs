f5aac92ae580a25aa7cfd06eeb026a89417d3cbb
Yanato <mikadu355@yahoo.fr>
2016-05-10 19:34:34 +0200

    level design

--- Assets/Scripts/GUI - Menu/Texte.cs
+++ Assets/Scripts/GUI - Menu/Texte.cs
@@ -21,24 +21,25 @@ public class Texte : MonoBehaviour {
             n = n+1;
                switch(n) {
                 case 1:
-				    texte.text = "Celui-ci ne savait pas ce qu'il faisait là. \n En plus il était nue ! Mais sa c'est une autre histoire...";
+				    texte.text = "Celui-ci ne savait pas ce qu'il faisait là. \n En plus il était nu ! Mais ça c'est une autre histoire...";
                     break;
                 case 2:
-				texte.text = "Alors qu'il reprenait ces esprits, \n Ceux-ci ce mirent à lui parler. \n Ces voix tantôt douces tantôt puissantes \n l'incitait à faire du mal à la nature autour de lui." ;
+				texte.text = "Alors qu'il reprenait ces esprits, \n Ceux-ci se mirent à lui parler. \n Ces voix tantôt douces tantôt puissantes \n l'incitaient à faire du mal à la nature autour de lui." ;
                     break;
                 case 4:
-				texte.text = " Il trouva une torche face à lui.\n La flamme lui parla, le réchauffa, le réconforta  \n mais l'entraina par la même occassion\n dans la folie et dans la déchéance de son âme.";
+				texte.text = " Il trouva une torche face à lui.\n La flamme lui parla, le réchauffa, le réconforta  \n mais l'entraina par la même occasion\n dans la folie et dans la déchéance de son âme.";
                     break;
                 case 5:
-				texte.text = "La natura lui sembla alors hostile :\n - Les lapin eurent tout à coup les yeux luisant. \n - Il pouvait voir des visages sur les fleurs.\n - Les canards avaient un comportement étrange. \n\n  TOUT CES ANIMAUX VOULAIENT SA MORT !";
+				texte.text = "La natura lui sembla alors hostile :\n - Les lapins eurent tout à coup les yeux luisants. \n - Il pouvait voir des visages sur les fleurs.\n - Les canards avaient un comportement étrange. \n\n  TOUS CES ANIMAUX VOULAIENT SA MORT !";
                     break;
 
 			    case 7:
-				    texte.text = "Il avait sombré dans la folie ! \n Mais ne l'était-il pas déjà avant ? \n \n Tout ce qu'il savait,\n c'est que la réponse se tennait entre ses mains. ";
+				    texte.text = "Il avait sombré dans la folie ! \n Mais ne l'était-il pas déjà avant ? \n \n Tout ce qu'il savait,\n c'est que la réponse se tenait entre ses mains. ";
+					burnemSound ();
+
 				    break;
 			case 8:
 				texte.text = "Il comprit vite que le feu était\n l'unique solution à tout ces problèmes.\n \n Toutes les voix se mirent à chuchoter, murmurer,\n parler, crier, hurler la même chose... ";
-				burnemSound ();
 				    break;			
                  case 9:
 				    texte.text = "BURM'EM ALL.";
